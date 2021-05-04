f5684b6326f518cac05aea3ed5093b58ab10f902
Aromee Kim <aromeekim@aromees-mbp.home>
2021-05-03 23:28:19 -0700

    Fixes Authentication

---
+++ src/Pages/OrderHistoryPage/OrderHistoryPage.jsx
@@ -0,0 +1,14 @@
+import * as usersService from '../../utilities/users-service';
+
+export default function OrderHistoryPage() {
+    async function handleCheckToken() {
+        usersService.checkToken(); 
+    }
+    
+    return (
+        <>
+        <h1>OrderHistoryPage</h1>
+        <button onClick={handleCheckToken}>Check When My Login Expires</button>
+        </>
+    )
+}
